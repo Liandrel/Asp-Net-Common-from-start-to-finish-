994940edb1c2d98e864030520f38b8838b9f1413
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-25 18:50:14 +0200

    class models
    created Food and OrderModel files

--- DataLibrary/Models/FoodModel.cs
+++ DataLibrary/Models/FoodModel.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace DataLibrary.Models
 {
-    internal class FoodModel
+    public class FoodModel
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
     }
 }
--- DataLibrary/Models/OrderModel.cs
+++ DataLibrary/Models/OrderModel.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataLibrary.Models
 {
-    internal class OrderModel
+    public class OrderModel
     {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20, ErrorMessage = "You need to keep the name to max of 20 characters")]
+        [MinLength(3, ErrorMessage ="You need to enter at least 3 characters for order name")]
+        [DisplayName("Name for the Order")]
+        public string OrderName { get; set; }
+
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        [Range(1,int.MaxValue, ErrorMessage = "You need to select a meal from the list")]
+        [DisplayName("Meal")]
+        public int FoodId { get; set; }
+
+        [Required]
+        [Range(1,10,ErrorMessage = "You can order maximum of 10 meals at once")]
+        public int Quantity { get; set; }
+
+        public decimal Total { get; set; }
+
     }
 }
