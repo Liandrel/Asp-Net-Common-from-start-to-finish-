a534975a8d3bfd2d7fbf1db3178ec06df425efac
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-28 18:56:49 +0200

    Models Update

--- DataLibrary/Models/FoodModel.cs
+++ DataLibrary/Models/FoodModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace DataLibrary.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+
+        [DisplayFormat(DataFormatString = ("{0:C}"), ApplyFormatInEditMode = true)]
         public decimal Price { get; set; }
 
     }
--- DataLibrary/Models/OrderModel.cs
+++ DataLibrary/Models/OrderModel.cs
@@ -15,20 +15,21 @@ namespace DataLibrary.Models
         [Required]
         [MaxLength(20, ErrorMessage = "You need to keep the name to max of 20 characters")]
         [MinLength(3, ErrorMessage ="You need to enter at least 3 characters for order name")]
-        [DisplayName("Name for the Order")]
+        [Display(Name = "Name for the Order")]
         public string OrderName { get; set; }
 
         public DateTime OrderDate { get; set; } = DateTime.UtcNow;
 
         [Required]
         [Range(1,int.MaxValue, ErrorMessage = "You need to select a meal from the list")]
-        [DisplayName("Meal")]
+        [Display(Name = "Meal")]
         public int FoodId { get; set; }
 
         [Required]
         [Range(1,10,ErrorMessage = "You can order maximum of 10 meals at once")]
         public int Quantity { get; set; }
 
+        [DisplayFormat(DataFormatString = ("{0:C}"), ApplyFormatInEditMode = true)]
         public decimal Total { get; set; }
 
     }
